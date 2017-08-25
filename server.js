aa72dca9639e332d9cbc4d37cd929f4db0a85227
Thomas Mercer <tmercer74@gmail.com>
2017-08-25 14:29:46 -0700

    updated js files

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/newsScrape ");
+mongoose.connect("mongodb://localhost/newsScrape");
 var db = mongoose.connection;
 
 // Show any mongoose errors
